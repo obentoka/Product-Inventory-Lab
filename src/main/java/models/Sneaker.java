640cd2966b30f12df93f8dfe24ecf15c209cba8a
vle <obentoka@gmail.com>
2020-02-13 09:24:05 -0500

    finished setting up fields

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -17,11 +17,43 @@ public class Sneaker {
         return this.name;
     }
 
-    public void setBrand(String name){
-        this.brand = name;
+    public void setBrand(String brand){
+        this.brand = brand;
     }
 
     public String getBrand(){
         return this.brand;
+    }
+
+    public void setSport(String sport){
+        this.sport = sport;
+    }
+
+    public String getSport(){
+        return this.sport;
+    }
+
+    public void setSize(float size){
+        this.size = size;
+    }
+
+    public float getSize(){
+        return this.size;
+    }
+
+    public void setQuantity(int quantity){
+        this.qty = quantity;
+    }
+
+    public int getQuantity(){
+        return this.qty;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public float getPrice(){
+        return this.price;
     }
 }
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -38,9 +38,48 @@ public class SneakerTest {
 
         // when (2)
         Sneaker testSneaker = new Sneaker();
-        testSneaker.setName(expected);
+        testSneaker.setSport(expected);
 
         // then (3)
-        Assertions.assertEquals(expected, testSneaker.getName());
+        Assertions.assertEquals(expected, testSneaker.getSport());
+    }
+
+    @Test
+    public void setSizeTest() {
+        // given (1)
+        float expected = 8.0f;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSize(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getSize());
+    }
+
+    @Test
+    public void setQuantityTest() {
+        // given (1)
+        int expected = 45;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setQuantity(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getQuantity());
+    }
+
+    @Test
+    public void setPriceTest() {
+        // given (1)
+        float expected = 100.0f;
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setPrice(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getPrice());
     }
 }
