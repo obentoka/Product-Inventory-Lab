fe71983314ced14e63f0de810925210c16355770
vle <obentoka@gmail.com>
2020-02-14 17:03:10 -0500

    setter and getter test done

--- src/main/java/models/Belt.java
+++ src/main/java/models/Belt.java
@@ -1,4 +1,53 @@
 package models;
 
 public class Belt {
+
+    private String brand;
+    private String buckle;
+    private String color;
+    private int size;
+    private float price;
+
+    public Belt() {
+    }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public String getBuckle() {
+        return buckle;
+    }
+
+    public void setBuckle(String buckle) {
+        this.buckle = buckle;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
